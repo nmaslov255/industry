cb4951542bee3f5b02b2c53ae7266a8a8675db06
nmaslov <nmaslov255@yandex.ru>
2020-09-29 22:03:02 +0300

    move static files to base_dir/static

--- industry/industry/settings.py
+++ industry/industry/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@dsrlv2u-n8oimb@*2!xl*zo^e^+hy-@d1o&f4xjptr$lru-t0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -117,5 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATIC_URL = '/static/'
