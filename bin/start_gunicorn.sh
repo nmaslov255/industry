b9740f2e71756db041141cf8e2b9664b1f0a56b8
nmaslov <nmaslov255@yandex.ru>
2020-09-29 11:22:56 +0300

    added gunicorn

---
+++ bin/start_gunicorn.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /home/user/industry/env/bin/activate
+source /home/user/industry/env/bin/postactivate
+exec gunicorn -c "/home/user/industry/industry/gunicorn_config.py" industry.wsgi
---
+++ industry/gunicorn_config.py
@@ -0,0 +1,8 @@
+command = '/home/user/industry/env/bin/gunicorn'
+pythonpath = '/home/user/industry/config'
+bind = '127.0.0.1:8001'
+workers = 3
+user = 'user'
+limit_requests_fields = 3200
+limit_requests_field_size = 0
+raw_env = 'DJANGO_SETTINGS_MODULE=config.settings'
