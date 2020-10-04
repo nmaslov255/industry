26815d63aadd38ebf50028f9a008d46f6601810d
nmaslov <nmaslov255@yandex.ru>
2020-10-05 00:54:49 +0300

    added ajax load posts

--- industry/static/js/main.js
+++ industry/static/js/main.js
@@ -1,6 +1,29 @@
-
 $('.menuu .button').click(function(event) {
     $(this).toggleClass('active');
     $('.burger').toggleClass('active');
     return false;
-  });
+});
+
+$('.bottom-field .news__read.bttn').click(function(event) {
+    let $btn = $(this)
+
+    let $newsBlock = $(this).closest('.news__item');
+    let $dateBlock = $newsBlock.find('.date__text');
+    let $itemBlock = $newsBlock.find('.news__one');
+
+    let date = $dateBlock.attr('date');
+    let items_length = $itemBlock.length;
+
+    let limit = items_length+10;
+
+    $.ajax({
+        url: 'crypto_ajax/'+date+'/'+limit,
+        success: function(responce){
+            $newsBlock.find('.items__box').html(responce);
+
+            if ($newsBlock.find('.news__one').length != limit){
+                $btn.closest('.bottom-field').hide()
+            };
+        }
+    });
+});
