9f6731be2bccec8862ea6e7512ceb96aa1186fb9
nmaslov <nmaslov255@yandex.ru>
2020-10-05 16:48:37 +0300

    fix post order

--- industry/main/services/CRUD.py
+++ industry/main/services/CRUD.py
@@ -3,6 +3,6 @@ from .helpers import datetime_calendar
 
 
 def get_posts(start_date, end_date, category_id):
-    return Post.objects.order_by('-pub_date')\
+    return Post.objects.order_by('-pub_date', '-id')\
                .filter(category=category_id, pub_date__gte=start_date)\
                .exclude(pub_date__gte=end_date)
