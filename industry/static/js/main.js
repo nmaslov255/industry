ec13270ef2a58e99ed4a866a3ab63a09259b9307
nmaslov <nmaslov255@yandex.ru>
2020-11-27 03:14:58 +0300

    added slide animation & upd currency by time interval & color highlighting

--- industry/static/js/main.js
+++ industry/static/js/main.js
@@ -28,14 +28,47 @@ $('.bottom-field .news__read.bttn').click(function(event) {
     });
 });
 
-$.ajax({
-    url: 'https://api.coingecko.com/api/v3/simple/price',
-    data: {'ids': 'etoro-euro,tether,bitcoin,ethereum', 
-           'vs_currencies': 'rub'},
-    success: function(responce){
-        $('#usdt-currency').text(responce['tether']['rub'])
-        $('#etoro-euro-currency').text(responce['etoro-euro']['rub'])
-        $('#bitcoin-currency').text(responce['bitcoin']['rub'])
-        $('#ethereum-currency').text(responce['ethereum']['rub'])
+function changePriceTicker($el, newPrice){
+    var lastPrice = parseFloat($el.text());
+
+    if (newPrice > lastPrice){
+        $el.css('color','rgb(2, 192, 118)');
+    } else if (newPrice < lastPrice) {
+        $el.css('color','rgb(248, 73, 96)');
+    } else {
+        $el.css('color', 'black');
     }
-})
+
+    $el.text(newPrice)
+}
+
+function loadTicker(){
+    $.ajax({
+        url: 'https://api.coingecko.com/api/v3/simple/price',
+        data: {'ids': 'etoro-euro,tether,bitcoin,ethereum', 
+               'vs_currencies': 'rub'},
+        success: function(responce){
+            $('#usdt-currency').slideUp(600, callback=() => {
+                var $el = $('#usdt-currency');
+                changePriceTicker($el, responce['tether']['rub']);
+            }).slideDown(600)
+
+            $('#etoro-euro-currency').slideUp(600, callback=() => {
+                var $el = $('#etoro-euro-currency');
+                changePriceTicker($el, responce['etoro-euro']['rub']);
+            }).slideDown(600)
+
+            $('#bitcoin-currency').slideUp(600, callback=() => {
+                var $el = $('#bitcoin-currency');
+                changePriceTicker($el, responce['bitcoin']['rub']);
+            }).slideDown(600)
+
+            $('#ethereum-currency').slideUp(600, callback=() => {
+                var $el = $('#ethereum-currency');
+                changePriceTicker($el, responce['ethereum']['rub']);
+            }).slideDown(600)
+        }
+    })
+}
+loadTicker()
+setInterval(loadTicker, 1000*60)
